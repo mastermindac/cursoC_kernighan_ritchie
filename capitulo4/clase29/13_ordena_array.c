3986301bd7c97f80b8cd277cf840503770277e04
Paco <paco@mastermind.ac>
2024-10-01 12:01:16 +0200

    Clase 29. Algoritmo de ordenacion shell

---
+++ capitulo4/clase29/13_ordena_array.c
@@ -0,0 +1,41 @@
+/*
+    CURSO C LIBRO KERNIGHAN & RITCHIE
+    ordenacion ascendente
+
+    ordenación de un array de forma ascendente
+    herramientas: funciones, for
+*/
+
+#include <stdio.h>
+
+void shellshort(int v[],int n);
+
+int main(){
+    int i;
+    int lista_desordenada[]={3,1,675,90,6,120,469,220,875};
+
+    /* Imprimimos la lista desordenada */
+    printf("---------- LISTA DESORDENADA ---------\n");
+    for(i=0;i<9;++i) printf("%d ",lista_desordenada[i]);
+    /*busqueda binaria*/
+    shellshort(lista_desordenada,9);
+
+    /* Imprimimos la lista ordenada */
+    printf("\n---------- LISTA ORDENADA ---------\n");
+    for(i=0;i<9;++i) printf("%d ",lista_desordenada[i]);
+    /* Imprimimos la lista ordenada */
+}
+
+/*shellshort: ordena v[]*/
+void shellshort(int v[],int n){
+    int gap,i,j,temp;
+
+    for(gap=n/2;gap>0;gap/=2)
+        for(i=gap;i<n;i++)
+            for(j=i-gap;(j>=0)&&(v[j]>v[j+gap]);j-=gap){
+                temp=v[j];
+                v[j]=v[j+gap];
+                v[j+gap]=temp;
+            }
+
+}
